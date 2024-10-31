f9d85a64c49b2c0697047a426cf89fa585d867ce
MRSgkce <mursidegokce985@gmail.com>
2024-10-31 13:24:51 +0300

    Initial Commit

---
+++ vacumm2/vacumm2App.swift
@@ -0,0 +1,17 @@
+//
+//  vacumm2App.swift
+//  vacumm2
+//
+//  Created by Mürşide Gökçe on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct vacumm2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
